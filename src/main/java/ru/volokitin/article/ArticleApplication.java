6640481ca2f0377ca78a11f267f7207ce73f8d4c
Волокитин Дмитрий Витальевич <volokitindv@admlr.lipetsk.ru>
2022-09-17 13:38:31 +0300

    commit

---
+++ src/main/java/ru/volokitin/article/ArticleApplication.java
@@ -0,0 +1,13 @@
+package ru.volokitin.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+	}
+
+}
